5185e30cfe7050ad1106749d6b268c8425e32c18
Danny <dsharris85@gmail.com>
2020-02-05 16:06:01 -0600

    Update test_visualization.py
    cleaned up comments

--- Demo/test_visualization.py
+++ Demo/test_visualization.py
@@ -11,19 +11,7 @@ def main():
         cloud = pcl.load(sys.argv[1])
     else:
         cloud = pcl.load("square.pcd")    
-    '''
-    # K-D Tree
-    tree = cloud.make_kdtree()
-    mls = cloud.make_moving_least_squares()
-
-    mls.set_Compute_Normals(True)
-    mls.set_polynomial_fit(True)
-    mls.set_Search_Method(tree)
-    mls.set_search_radius(0.05)
-    mls_points = mls.process()
-    pcl.save_PointNormal(mls_points, 'mls_test.pcd')
-    #cloud2 = mls_points
-    '''
+   
     # Display Text
     hello = "Hello World!"
     viewer.AddText(str.encode(hello),10,50,bytes(1),0)
