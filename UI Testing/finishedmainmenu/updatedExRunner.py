fbcd70f562429acc8c5a32c1ee0325968f9e4755
kamelakapllani <54785460+kamelakapllani@users.noreply.github.com>
2020-04-30 01:15:18 -0500

    update of the menu
    added some more commenting to the finishedmain menu and added capture data widget.
    Also edited updatedexRunner.py to connect it to the capture data widget

--- UI Testing/finishedmainmenu/updatedExRunner.py
+++ UI Testing/finishedmainmenu/updatedExRunner.py
@@ -4,6 +4,7 @@ from updatedEx import MainMenu
 import sys
 import subprocess
 import time #Only for testing closing and reopening the window
+from CaptureDataWidget import Ui_CaptureData
 
 class MyMenu(QtWidgets.QDialog):
     def __init__(self):
@@ -19,21 +20,46 @@ class MyMenu(QtWidgets.QDialog):
         self.ui.Deselect.clicked.connect(self.deselect)
         self.ui.ViewPointCloud.clicked.connect(self.file_open1)
         self.ui.ViewMesh.clicked.connect(self.file_open2)
-
+        
     def capture(self):
         # Hide window
         self.hide()
 
         # do something
-        time.sleep(2)
-
+        time.sleep(1)
+        # self.objectname=Kamy'sDialog()
+        # test = self.objectname.exec_()
+        # exec_ is a blocking action.
+        #connect the Capture Data interface with the main menu
+        self.window=QtWidgets.QMainWindow()
+        self.ui=Ui_CaptureData()
+        self.ui.setupUi(self.window)
+        self.hide()
+        self.window.show()
+        #open a file from an existing directory
+        self.ui.fromanexistingDirectory.clicked.connect(self.file_open3)
+        #self.ui.Deselect.clicked.connect(self.deselect_2)
+        self.ui.backtomainmenu.clicked.connect(self.backtomenu)
+        #opening by a typed directory 
+        # self.ui.commandLinkButton.clicked.connect(self.?)
         # self.objectname=Kamy'sDialog()
         # test = self.objectname.exec_()
         # exec_ is a blocking action. 
 
         # Re-open UI
-        self.open()
+     #  self.open()
+     # Capture Data From an existing directory - Requires opening a file/folder?
+    def file_open3(self):
+        dirName = QFileDialog.getExistingDirectory()
+        
 
+       #def deselect_2(self):
+        # update deselect_2 for deselecting in capturedatawidget
+       # self.ui.Reg.setText("Hi")
+    def backtomenu(self):
+        self.window.hide()
+        self.show()
+        
     def registration(self):
         # call manual reg
         self.ui.Reg.setText("Hi")
@@ -48,6 +74,7 @@ class MyMenu(QtWidgets.QDialog):
     def deselect(self):
         dirName = ""
         self.ui.DirText.setText("Directory Selected: " + dirName)
+        
         self.ui.Reg.setEnabled(False)
         self.ui.Deselect.setEnabled(False)
 
