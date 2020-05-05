a637de6052a1588317b76911bc37e3ea63599adb
Knogo <57305222+Knogo@users.noreply.github.com>
2020-05-05 16:34:30 -0500

    updated the runner with more functionality and Kamy's window as a seperate class

--- UI Testing/finishedmainmenu/updatedExRunner.py
+++ UI Testing/finishedmainmenu/updatedExRunner.py
@@ -1,10 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
-from updatedEx import MainMenu
+import os.path
 import sys
 import subprocess
 import time #Only for testing closing and reopening the window
-from CaptureData import Ui_CaptureData
+from updatedEx import MainMenu
+from CaptureDataEdited import Ui_CaptureData
 
 class MyMenu(QtWidgets.QDialog):
     def __init__(self):
@@ -12,6 +13,7 @@ class MyMenu(QtWidgets.QDialog):
         self.ui = MainMenu()
         self.ui.setupUi(self)
         self.show()
+        self.dirName = ""
 
         # Define button actions to functions 
         self.ui.CaptureData.clicked.connect(self.capture)
@@ -25,63 +27,32 @@ class MyMenu(QtWidgets.QDialog):
         # Hide window
         self.hide()
 
-        # do something
-        time.sleep(1)
-        # self.objectname=Kamy'sDialog()
-        # test = self.objectname.exec_()
-        # exec_ is a blocking action.
-        #connect the Capture Data interface with the main menu
-        
-        self.CaptureDataForm=QtWidgets.QDialog()
-        self.ui=Ui_CaptureData()
-        self.ui.setupUi(CaptureDataForm)
-        self.hide()
-        self.CaptureData.show()
-        
-        #open a file from an existing directory
-        self.ui.LoadDir_3.clicked.connect(self.file_open3)
-        #self.ui.Deselect.clicked.connect(self.deselect_2)
-        self.ui.Mainmenu.clicked.connect(self.backtomenu)
-        #opening by a typed directory 
-        # self.ui.commandLinkButton.clicked.connect(self.?)
-        # self.objectname=Kamy'sDialog()
-        # test = self.objectname.exec_()
-        # exec_ is a blocking action. 
-        
-        # Re-open UI
-     #  self.open()
-     # Capture Data From an existing directory - Requires opening a file/folder?
-   
-        
-        
-    
-    def file_open3(self):
-        dirName = QFileDialog.getExistingDirectory()
-        
+        self.popup = CaptureDataDialog(self.dirName)
+        test = self.popup.exec_()
+        if (test):
+            self.dirName = self.popup.dirName
+            self.ui.DirText.setText("Directory Selected: " + self.dirName)
+            self.ui.Reg.setEnabled(True)
+            self.ui.Deselect.setEnabled(True) 
 
-       #def deselect_2(self):
-        # update deselect_2 for deselecting in capturedatawidget
-       # self.ui.Reg.setText("Hi")
-    
+            #call main(dirName)
 
-    def backtomenu(self):
-        self.dialog.hide()
-        self.show()
+        self.open()
         
     def registration(self):
         # call manual reg
         self.ui.Reg.setText("Hi")
 
     def dir_open(self):
-        dirName = QFileDialog.getExistingDirectory()
-        self.ui.DirText.setText("Directory Selected: " + dirName)
-        if (dirName != ""): # i.e. they exit without selecting a directory
+        self.dirName = QFileDialog.getExistingDirectory()
+        self.ui.DirText.setText("Directory Selected: " + self.dirName)
+        if (self.dirName != ""): # i.e. they exit without selecting a directory
             self.ui.Reg.setEnabled(True) # Reg should be allowed only when a directory is selected
             self.ui.Deselect.setEnabled(True) # Deselect should be allowed only when a directory is selected
 
     def deselect(self):
-        dirName = ""
-        self.ui.DirText.setText("Directory Selected: " + dirName)
+        self.dirName = ""
+        self.ui.DirText.setText("Directory Selected: " + self.dirName)
         
         self.ui.Reg.setEnabled(False)
         self.ui.Deselect.setEnabled(False)
@@ -97,6 +68,43 @@ class MyMenu(QtWidgets.QDialog):
         fileName = QFileDialog.getOpenFileName()
         # if (fileName != ""): # i.e. they exit without selecting a file
             # do something with file
+
+class CaptureDataDialog(QtWidgets.QDialog):
+    def __init__(self, givendirName):
+        super(CaptureDataDialog, self).__init__()
+        self.ui = Ui_CaptureData()
+        self.ui.setupUi(self)
+        self.show()
+
+        self.ui.LoadDir_3.clicked.connect(self.dir_open)
+        self.ui.Deselect.clicked.connect(self.deselect)
+        self.ui.lineEdit.textChanged.connect(self.textchanged)
+        self.ui.pushButton.clicked.connect(self.select)
+
+        self.dirName = givendirName
+        self.ui.lineEdit.setText(self.dirName)
+
+    def dir_open(self):
+        self.dirName = QFileDialog.getExistingDirectory()
+        self.ui.lineEdit.setText(self.dirName)
+
+    def deselect(self):
+        self.dirName = ""
+        self.ui.lineEdit.setText(self.dirName)
+        self.ui.Deselect.setEnabled(False)
+        
+    def textchanged(self):
+        if (self.ui.lineEdit.text() != ""):
+            self.ui.Deselect.setEnabled(True)
+
+    def select(self):
+        self.dirName = self.ui.lineEdit.text()
+
+        if not(os.path.isdir(self.dirName)):
+            self.ui.DirText.setText("You did not enter in a valid directory.")
+        else:
+            self.ui.DirText.setText("")
+            self.accept()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
